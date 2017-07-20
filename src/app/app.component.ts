e500b4780fab5f15a4f4cc157452b9253f8c7e5c
EC2 Default User <ec2-user@ip-172-31-21-21.ap-southeast-2.compute.internal>
2017-07-20 07:16:38 +0000

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,86 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'My Angular Calculator';
+  a:string;
+  b:string;
+  operator:string;
+
+  total = 0;
+  output = '0';
+  newOutput = true;
+
+  pendingValue = null;
+  runningTotal = null;
+  pendingOperation = null;
+
+  public updateOutput(value){
+     if (this.newOutput){
+       this.output = value;
+       this.newOutput = false;
+     }else{     
+       if (this.output == '0' && value == '0'){
+          this.output = value;
+       }else{
+	if (this.output.search(/\./) < 0 || value != '.'){
+	  this.output += value;
+        }
+       } 
+     }
+     this.pendingValue = parseFloat(this.output);
+  }
+
+  public clearOutput(){
+     this.newOutput = true;
+     this.runningTotal = null;
+     this.pendingValue = null;
+     this.pendingOperation = null; 
+     this.output = '0';
+  }
+
+  public operation(operatorTemp){     
+     if (this.runningTotal == null){
+	this.runningTotal = this.pendingValue;
+     }else{
+	this.calculateTotal();
+     }
+
+     if (operatorTemp == 'sqrt'){
+        this.pendingOperation = '√';
+     }else{
+        this.pendingOperation = operatorTemp;
+     }
+
+     this.newOutput = true;     
+  }
+
+  public calculateTotal(){
+     if (this.pendingOperation == '+'){
+	this.runningTotal += this.pendingValue;
+     }
+     if (this.pendingOperation == '-'){
+        this.runningTotal -= this.pendingValue;
+     }
+     if (this.pendingOperation == '*'){
+        this.runningTotal *= this.pendingValue;
+     }
+     if (this.pendingOperation == '/'){
+        this.runningTotal /= this.pendingValue;
+     }
+     if (this.pendingOperation == '^'){
+        this.runningTotal = Math.pow(this.runningTotal,this.pendingValue);
+     }
+     if (this.pendingOperation == '√'){
+        this.runningTotal = Math.sqrt(parseInt(this.pendingValue));;
+     }
+     if(this.runningTotal == null){
+         this.runningTotal = this.pendingValue;
+     }
+     this.output = this.runningTotal;
+     this.newOutput = true;
+  }
+
 }
+
